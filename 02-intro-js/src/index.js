a1095d2551b6f1cc7f7fc6e4fb6dd08062bdbe4d
PrintHolamundo <jdohca@gmail.com>
2021-11-18 09:37:05 -0600

    Async-Await

--- 02-intro-js/src/index.js
+++ 02-intro-js/src/index.js
@@ -1,31 +1,3 @@
 
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
-
-    try {
-
-        const apiKey = 'vwTWx0iCJQVVT2uEqZD1hO3sxmxIp2eb';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
+console.log(    'Hello World'    );
